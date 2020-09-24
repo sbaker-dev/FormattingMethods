b2ab376668ac5c9c893e405891dce432e678275b
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-09-24 10:35:52 +0100

    Add check Date

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ MAINTAINER = 'Samuel Baker'
 MAINTAINER_EMAIL = 'samuelbaker.researcher@gmail.com'
 LICENSE = 'MIT'
 DOWNLOAD_URL = "https://github.com/sbaker-dev/FormattingMethods"
-VERSION = "0.01.0"
+VERSION = "0.02.0"
 PYTHON_REQUIRES = ">=3.6"
 
 INSTALL_REQUIRES = [
