a489fedb49ff45b13f724a53c2723cc041663e8f
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-03-10 09:55:03 +0000

    Updating for new format

--- FormattingMethods/__init__.py
+++ FormattingMethods/__init__.py
@@ -1,3 +1,3 @@
-from FormattingMethods.rates_and_dates import *
-from FormattingMethods.file_helpers import *
-from miscSupports import *
+from FormattingMethods.manipulate_dates import check_date, within_date, rebase_year
+from FormattingMethods.file_helpers import extract_headers, write_chunks
+from FormattingMethods.exposures import assigned_exposure
