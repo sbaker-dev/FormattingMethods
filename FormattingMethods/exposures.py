fb6773c0ef9774e1c0d4dab87d77a9b70ec23251
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-03-11 15:30:59 +0000

    Update to allow for multiple phenotypes and catch missing data for exposures

--- FormattingMethods/exposures.py
+++ FormattingMethods/exposures.py
@@ -1,4 +1,5 @@
 from miscSupports import flatten, chunk_list
+import numpy as np
 
 
 def assigned_exposure(unique_place_list, phenotype_keys, database, age, average_exposures=None,
@@ -9,7 +10,7 @@ def assigned_exposure(unique_place_list, phenotype_keys, database, age, average_
     :param unique_place_list: A list of unique places dates where each element is year__month__gid__place, deliminator
         is by default __ but can be overridden via place_delimiter
     :type unique_place_list: list[str]
-        
+
     :param place_delimiter: The delimiter to be used to split a place in unique_place_list
     :type place_delimiter: str
 
@@ -45,29 +46,31 @@ def assigned_exposure(unique_place_list, phenotype_keys, database, age, average_
         birth_date = year + month + "01"
         place_data = database[f"{gid}__{place}"]
 
+        phenotype_values = []
         for phenotype in phenotype_keys:
+            # Check that we have information for this phenotype
+            try:
+                place_phenotype = place_data[phenotype]
+                place_population = place_data[population_name]
+            except KeyError:
+                phenotype_values.append(_assign_failed(average_exposures, age))
+                continue
 
             # Divide each year into months, then start from the month of birth a sum the month population values into
             # new years
-            relevant_pop = [pop for date, pop in place_data[population_name].items() if date[:4] >= year]
+            relevant_pop = [pop for date, pop in place_population.items() if date[:4] >= year]
             relevant_pop = flatten([[pop / 12 for _ in range(12)] for pop in relevant_pop])[age:]
             relevant_pop = [sum(year_pop) for year_pop in chunk_list(relevant_pop, 12) if len(year_pop) == 12][:age]
 
             # isolate the weekly values then chunk them into years
-            phenotype_weeks = [v for date, v in place_data[phenotype].items() if date >= birth_date]
-
-            # Chunk them into years
+            phenotype_weeks = [v for date, v in place_phenotype.items() if date >= birth_date]
             phenotype_years = chunk_list(phenotype_weeks, 52)
             phenotype_years = [sum(week_chunk) if i < age and len(week_chunk) == 52 else "NA"
                                for i, week_chunk in enumerate(phenotype_years)][:age]
 
             # If we do not have a valid set of answers, return NA for each row that could be set
             if (len(phenotype_years) < age) or ("NA" in phenotype_years) or len(relevant_pop) < age:
-                if average_exposures:
-                    link_ref_dict[f"{year}__{month}__{gid}__{place}"] = ["NA" for _ in range(age)] + \
-                                                                        ["NA" for _ in range(len(average_exposures))]
-                else:
-                    link_ref_dict[f"{year}__{month}__{gid}__{place}"] = ["NA" for _ in range(age)]
+                phenotype_values.append(_assign_failed(average_exposures, age))
 
             # Otherwise calculate the exposure at each given age up to the total of age and averages, if set, by each
             # age in the average_exposure list.
@@ -80,10 +83,20 @@ def assigned_exposure(unique_place_list, phenotype_keys, database, age, average_
                     # If we want the average by a given age, iterate the totals up to the current age to determine the
                     # average exposure
                     if average_exposures and (i + 1 in average_exposures):
-                        average = (sum([phenotype_years[ii] for ii in range(i + 1)]) / sum(
-                            relevant_pop[ii] for ii in range(i + 1))) * rate_per
-                        averages.append(average)
+                        av_exposure = np.average([phenotype_years[ii] for ii in range(i + 1)])
+                        av_pop = np.average([relevant_pop[ii] for ii in range(i + 1)])
+                        averages.append((av_exposure / av_pop) * rate_per)
 
-                link_ref_dict[f"{year}__{month}__{gid}__{place}"] = exposures + averages
+                phenotype_values.append(exposures + averages)
+
+        link_ref_dict[f"{year}__{month}__{gid}__{place}"] = phenotype_values
 
     return link_ref_dict
+
+
+def _assign_failed(average_exposures, age):
+    """If we fail to information then this writes out the failed rows"""
+    if average_exposures:
+        return ["NA" for _ in range(age)] + ["NA" for _ in range(len(average_exposures))]
+    else:
+        return ["NA" for _ in range(age)]
